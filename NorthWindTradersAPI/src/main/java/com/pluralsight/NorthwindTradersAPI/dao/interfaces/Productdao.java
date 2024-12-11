7137e386ea0cb7eb7e284e65e9b08ec5f9592e1c
Djharness <djharness2@gmail.com>
2024-12-11 13:23:43 -0500

    Exercises

---
+++ NorthWindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/dao/interfaces/Productdao.java
@@ -0,0 +1,10 @@
+package com.pluralsight.NorthwindTradersAPI.dao.interfaces;
+
+import com.pluralsight.NorthwindTradersAPI.models.Product;
+
+import java.util.List;
+
+public interface ProductDao {
+    List<Product> getAll();
+    Product getByProductId(int id);
+}
